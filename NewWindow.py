e002bddc8be376bdd924594c4fe6aeacb43b3362
Derrickeee <44491830+Derrickeee@users.noreply.github.com>
2022-10-26 05:48:13 -0700

    Update NewWindow.py

--- NewWindow.py
+++ NewWindow.py
@@ -4,6 +4,16 @@ import time
 import datetime
 import webbrowser
 
+data = [{ 'blockNumber': '6141243',
+          'direction': 0,
+          'from': '0x7ed2d423ba4bc317e45561caa823b7d6cc2024a9',
+          'from_labels': None,
+          'hash': '0x1a99ef8d9ae7087e276451d460f9b100a3c667d07c69a4b95bcd0296132e5738',
+          'timestamp': '1534182274',
+          'to': '0x7129bed9a5264f0cf279110ece27add9b6662bd5',
+          'to_labels': None,
+          'value': 26.10836602777778}]
+          
 class NewWindow(QMainWindow):
     """
     Class definition for transaction window
@@ -12,18 +22,22 @@ class NewWindow(QMainWindow):
         super(NewWindow, self).__init__(parent)
         # Keep a local copy of the parent object
         self.homeparent = parent
-
+    
         # Load the Home Window UI design from file
         uic.loadUi("./basic.ui", self)
-        self.show()
-        
+
+
 def main():
     # Define the PyQT5 application object
     app = QApplication([])
 
     # Create the main window and show it
-    tw = NewWindow()
+    tw = NewWindow(data)
     tw.show()
 
     # Start the PyQT5 app
     app.exec_()
+
+
+if __name__ == "__main__":
+    main()
